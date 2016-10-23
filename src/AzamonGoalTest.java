91455f044970008bab22a3bece364c6becb34ef4
Javier Lopez Calderon <javierlopezcalderon@gmail.com>
2016-10-23 20:53:02 +0200

    CHANGE goalTest simplify to false

--- src/AzamonGoalTest.java
+++ src/AzamonGoalTest.java
@@ -4,8 +4,11 @@ import aima.search.framework.GoalTest;
  * Created by Javier Lopez on 19/10/16.
  */
 public class AzamonGoalTest implements GoalTest{
+
+    public AzamonGoalTest(){}
+
     @Override
     public boolean isGoalState(Object state) {
-        return ((AzamonState) state).isGoal();
+        return false;
     }
 }
