09608dc882b51c4f13127de16320715af30669ee
Javier Lopez Calderon <javierlopezcalderon@gmail.com>
2016-10-23 20:53:40 +0200

    CHANGE AzamonSuccessorFunction completed code

--- src/AzamonSuccessorFunction.java
+++ src/AzamonSuccessorFunction.java
@@ -6,21 +6,28 @@ import java.util.List;
 /**
  * Created by mycol on 20/10/16.
  */
-public class AzamonSuccessorFunction implements aima.search.framework.SuccessorFunction {
+public class AzamonSuccessorFunction implements aima.search.framework.SuccessorFunction{
+    public AzamonSuccessorFunction(){}
+
     @Override
     public List getSuccessors(Object o) {
         AzamonState state = (AzamonState)o;
         ArrayList retVal = new ArrayList();
-        for (int i = 0; i < state.numeroPaquetes() ; i++) {
-            AzamonState newState = new AzamonState(state);
-
-            //TODO aplicar todos los operadores y añadirlos a retVal
-
-            retVal.add(new Successor("", newState));
+        AzamonHeuristic heuristic = new AzamonHeuristic();
+        for(int i = 0; i < state.numeroPaquetes(); i++){
+            for(int j = 0; j < state.numeroTransportes(); ++j){
+                if(state.esMovible(i, j)){
+                    AzamonState newState = new AzamonState(state.getPaqueteEnOferta(), state.getPesoDisponibleOfertas(), state.getPaquetes(), state.getTransporte());
+                    newState.moverPaquete(i, j);
+                    retVal.add(new Successor("MOVER(" + i + ", " + j + ", coste: " + heuristic.getHeuristicValue(newState) + ")", newState));
+                }
+                if(state.esIntercambiable(i, j)){
+                    AzamonState newState = new AzamonState(state.getPaqueteEnOferta(), state.getPesoDisponibleOfertas(), state.getPaquetes(), state.getTransporte());
+                    newState.intercambiarPaquete(i, j);
+                    retVal.add(new Successor("INTERCAMBIO(" + i + ", " + j + ", coste: " + heuristic.getHeuristicValue(newState) + ")", newState));
+                }
+            }
         }
-
-
-
         return retVal;
     }
 }
