ca7924c3faf83b4d4915219a6e13a13c7cea1bbc
Javier Lopez Calderon <javierlopezcalderon@gmail.com>
2016-10-23 20:56:25 +0200

    ADD TODO notation in main file

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
         AzamonState azamonState = new AzamonState();
+        //TODO change initial state
         hillClimbingStrategy(azamonState);
         simulatedAnnealingStrategy(azamonState);
     }
