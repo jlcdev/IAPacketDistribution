74180b3fe07ac1c8179b04675432f1a90405b239
Javier Lopez Calderon <javierlopezcalderon@gmail.com>
2016-10-23 20:55:02 +0200

    CHANGE main file add simply start code, not completed

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,59 @@
-import aima.search.framework.Successor;
+import aima.search.framework.Problem;
+import aima.search.framework.SearchAgent;
+import aima.search.informed.HillClimbingSearch;
+import aima.search.informed.SimulatedAnnealingSearch;
 
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by Javier Lopez on 17/10/16.
  */
 public class Main {
+
     public static void main(String[] args) {
+        AzamonState azamonState = new AzamonState();
+        hillClimbingStrategy(azamonState);
+        simulatedAnnealingStrategy(azamonState);
+    }
+
+    private static void hillClimbingStrategy(AzamonState azamonState){
+        System.out.println("Azamon - Hill Climbing Selected");
+        try{
+            Problem problem = new Problem(azamonState, new AzamonSuccessorFunction(), new AzamonGoalTest(), new AzamonHeuristic());
+            HillClimbingSearch hillClimbingSearch = new HillClimbingSearch();
+            SearchAgent searchAgent = new SearchAgent(problem, hillClimbingSearch);
+            printAgentActions(searchAgent.getActions());
+            printAgentInstrumentation(searchAgent.getInstrumentation());
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void simulatedAnnealingStrategy(AzamonState azamonState){
+        System.out.println("Azamon - Simulated Annealing selected");
+        try{
+            Problem problem = new Problem(azamonState, new AzamonSuccessorFunction(), new AzamonGoalTest(), new AzamonHeuristic());
+            //TODO change simulated annealing paramateres
+            SimulatedAnnealingSearch simulatedAnnealingSearch = new SimulatedAnnealingSearch(2000, 100, 5, 0.001D);
+            SearchAgent searchAgent = new SearchAgent(problem, simulatedAnnealingSearch);
+            printAgentActions(searchAgent.getActions());
+            printAgentInstrumentation(searchAgent.getInstrumentation());
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void printAgentActions(List actions){
+        for(Object action : actions.toArray()){
+            System.out.println((String)action);
+        }
+    }
+
+    private static void printAgentInstrumentation(Properties properties){
+        properties.list(System.out);
+    }
+/*
+    public static void main2(String[] args) {
         int npaquetes;
         double proporcion;
         Scanner scan = new Scanner(System.in);
@@ -53,6 +100,6 @@ public class Main {
                     break;
             }
         }
-        //Successor e = new Successor("", new Object());
     }
+    */
 }
