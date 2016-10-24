24d0d38c6b191d71e3e761d1fedd3576ef262c1b
Javier Lopez Calderon <javierlopezcalderon@gmail.com>
2016-10-24 15:53:51 +0200

    FIX successor function

--- src/AzamonSuccessorFunction.java
+++ src/AzamonSuccessorFunction.java
@@ -14,17 +14,29 @@ public class AzamonSuccessorFunction implements SuccessorFunction{
         AzamonState state = (AzamonState)o;
         ArrayList retVal = new ArrayList();
         AzamonHeuristic heuristic = new AzamonHeuristic();
-        for(int i = 0; i < state.numeroPaquetes(); i++){
-            for(int j = 0; j < state.numeroTransportes(); ++j){
+        int nPaq = state.numeroPaquetes();
+        int nTrans = state.numeroTransportes();
+        int delta = nTrans + (nPaq - nTrans);
+        for(int i = 0; i < nPaq; i++){
+            for(int j = 0; j < nTrans; ++j){
                 if(state.esMovible(i, j)){
                     AzamonState newState = new AzamonState(state.getPaqueteEnOferta(), state.getPesoDisponibleOfertas(), state.getPaquetes(), state.getTransporte());
                     newState.moverPaquete(i, j);
-                    retVal.add(new Successor("MOVER(" + i + ", " + j + ", coste: " + heuristic.getHeuristicValue(newState) + ")", newState));
+                    retVal.add(new Successor("MOVER(paquete: " + i + ", oferta: " + j + ", coste: " + heuristic.getHeuristicValue(newState) + ")", newState));
                 }
+
                 if(state.esIntercambiable(i, j)){
                     AzamonState newState = new AzamonState(state.getPaqueteEnOferta(), state.getPesoDisponibleOfertas(), state.getPaquetes(), state.getTransporte());
                     newState.intercambiarPaquete(i, j);
                     retVal.add(new Successor("INTERCAMBIO(" + i + ", " + j + ", coste: " + heuristic.getHeuristicValue(newState) + ")", newState));
+                }
+            }
+
+            for(int k = nTrans; k < delta; ++k){
+                if(state.esIntercambiable(i, k)){
+                    AzamonState newState = new AzamonState(state.getPaqueteEnOferta(), state.getPesoDisponibleOfertas(), state.getPaquetes(), state.getTransporte());
+                    newState.intercambiarPaquete(i, k);
+                    retVal.add(new Successor("INTERCAMBIO(paquete:" + i + ", paquete:" + k + ", coste: " + heuristic.getHeuristicValue(newState) + ")", newState));
                 }
             }
         }
