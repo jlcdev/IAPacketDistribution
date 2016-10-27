83544065aa820b9ddac6945fe088e907f2ef200e
Albert Campano <acampano92@gmail.com>
2016-10-27 02:25:01 +0200

    CHANGE sort methods

--- src/AzamonState.java
+++ src/AzamonState.java
@@ -28,6 +28,8 @@ public class AzamonState {
         this.pesoDisponibleOfertas = oldPesoDisponibleOfertas.clone();
     }
 
+    //Funciones generadoras
+
     public void generateInitialStateSortPriority(int numPaq, int seedPaquetes, double proporcion, int seedOfertas){
         this.paquetes = new Paquetes(numPaq, seedPaquetes);
         this.transporte = new Transporte(this.paquetes, proporcion, seedOfertas);
@@ -43,7 +45,6 @@ public class AzamonState {
             }
         }
     }
-
     //Pone el paquete en el primer sitio valido
     public void generateInitialState(int numPaq, int seedPaquetes, double proporcion, int seedOfertas){
         this.paquetes = new Paquetes(numPaq, seedPaquetes);
@@ -59,7 +60,6 @@ public class AzamonState {
             }
         }
     }
-
     //Pone el paquete en un sitio aleatorio valido
     public void generateInitialStateRandom(int numPaq, int seedPaquetes, double proporcion, int seedOfertas){
         this.paquetes = new Paquetes(numPaq, seedPaquetes);
@@ -78,17 +78,11 @@ public class AzamonState {
         }
     }
 
+
+    //Condiciones de aplicabilidad
+
     public boolean esMovible(int pi, int oj){
-        return ( ((pesoDisponibleOfertas[oj] - paquetes.get(pi).getPeso()) > 0.0) && (prioridad(pi, oj) >= 0) );
-    }
-
-    public void moverPaquete(int pi, int oj){
-        double peso = paquetes.get(pi).getPeso();
-        int oi = paqueteEnOferta[pi];
-
-        paqueteEnOferta[pi] = oj;
-        pesoDisponibleOfertas[oi] += peso;
-        pesoDisponibleOfertas[oj] -= peso;
+        return ( ((pesoDisponibleOfertas[oj] - paquetes.get(pi).getPeso()) > 0.0) && (calcDiasFelicidad(pi, oj) >= 0) );
     }
 
     public boolean esIntercambiable(int pi, int pj) {
@@ -99,10 +93,22 @@ public class AzamonState {
         double pesoLibrei = pesoDisponibleOfertas[ofertai];
         double pesoLibrej = pesoDisponibleOfertas[ofertaj];
 
-        boolean condp = this.prioridad(pi, ofertaj) >= 0 && this.prioridad(pj, ofertai) >= 0;
+        boolean condp = this.calcDiasFelicidad(pi, ofertaj) >= 0 && this.calcDiasFelicidad(pj, ofertai) >= 0;
         boolean condi = (pesoLibrei + pesoi - pesoj) > 0.0;
         boolean condj = (pesoLibrej + pesoj - pesoi) > 0.0;
         return condi && condj && condp;
+    }
+
+
+    //Operaciones
+
+    public void moverPaquete(int pi, int oj){
+        double peso = paquetes.get(pi).getPeso();
+        int oi = paqueteEnOferta[pi];
+
+        paqueteEnOferta[pi] = oj;
+        pesoDisponibleOfertas[oi] += peso;
+        pesoDisponibleOfertas[oj] -= peso;
     }
 
     public void intercambiarPaquete(int pi, int pj){
@@ -120,43 +126,17 @@ public class AzamonState {
         pesoDisponibleOfertas[ofertaj] -= (pesoi - pesoj);
     }
 
-    @Override
-    public String toString() {
-        String s = "AzamonState{";
-
-        String spaq = "[";
-        for (Paquete p: this.paquetes) {
-            spaq += ("peso(" + p.getPeso() + ") pr(" +p.getPrioridad() + "), ");
+    private boolean ponerPaquete(int pi, int oj){
+        double deltaPeso = this.pesoDisponibleOfertas[oj] - this.paquetes.get(pi).getPeso();
+        int prioridad = calcDiasFelicidad(pi, oj);
+        if(deltaPeso > 0.0 && (prioridad >= 0 && prioridad <= 1)) {
+            this.paqueteEnOferta[pi] = oj;
+            this.pesoDisponibleOfertas[oj] = deltaPeso;
+            return true;
         }
-        spaq += "]";
-
-        String spaqof = "[";
-        for (Integer o: this.paqueteEnOferta)
-        {
-            spaqof += (o.toString() + ", ");
-        }
-        spaqof += "]";
-
-        String sof = "[";
-        for (Oferta of: this.transporte)
-        {
-            sof += ("pmax(" +of.getPesomax() +") dias(" +of.getDias() + "), ");
-        }
-        sof += "]";
-
-        String spof = "[";
-        for (Double peso: this.pesoDisponibleOfertas)
-        {
-            spof += (peso.toString() + ", ");
-        }
-        spof += "]";
-        return s + "\n" + spaq + "; \n" + spaqof + "; \n" + sof + "; \n" + spof + "}";
+        return false;
     }
 
-    public int numeroPaquetes () {
-        return this.paqueteEnOferta.length;
-    }
-    public int numeroTransportes(){ return this.transporte.size(); }
 
     /**
      * 0 = 0 -1 -2 -3 -4
@@ -168,44 +148,40 @@ public class AzamonState {
         return (1-dias) + (2*prioridad);
     }
 
-    //o.getDias() = {1, 2, 3, 4, 5}  p.getPrioridad() = {0, 1, 2}
-    //pr(0) = dias(1)   pr(1) = dias(2, 3)     pr(2) = dias(4, 5)
-    private int prioridad(int pi, int oj) {
-        int prioridad = this.paquetes.get(pi).getPrioridad(), dias = this.transporte.get(oj).getDias();
-        if (dias == 1) return prioridad;
-        else if(dias <= 3) return prioridad - 1;
-        else return prioridad - 2;
-    }
-
-    private boolean ponerPaquete(int pi, int oj){
-        double deltaPeso = this.pesoDisponibleOfertas[oj] - this.paquetes.get(pi).getPeso();
-        if(deltaPeso > 0.0 && (this.prioridad(pi, oj) == 0)) {
-            this.paqueteEnOferta[pi] = oj;
-            this.pesoDisponibleOfertas[oj] = deltaPeso;
-            return true;
-        }
-        return false;
-    }
-
     public double calcularPrecioPaqueteOferta(int paquete){
         Oferta o = this.getTransporte().get(this.getPaqueteEnOferta()[paquete]);
         double pesoPaq = this.getPaquetes().get(paquete).getPeso();
         return ((o.getPrecio() * pesoPaq) + (0.25 * ((o.getDias() == 1)?0:(o.getDias() > 1 && o.getDias() < 4)?1:2) * pesoPaq));
     }
 
+
+    //Funciones getters
+
+    public int numeroPaquetes () {
+        return this.paqueteEnOferta.length;
+    }
+    public int numeroTransportes(){ return this.transporte.size(); }
     public int[] getPaqueteEnOferta() {
         return paqueteEnOferta;
     }
-
     public double[] getPesoDisponibleOfertas() {
         return pesoDisponibleOfertas;
     }
-
     public Paquetes getPaquetes() {
         return paquetes;
     }
-
     public Transporte getTransporte() {
         return transporte;
+    }
+
+    @Override
+    public String toString() {
+        String s = "AzamonState{";
+        String spaq = this.paquetes.toString();
+        String spaqof = this.paqueteEnOferta.toString();
+        String sof = this.transporte.toString();
+        String spof = this.pesoDisponibleOfertas.toString();
+
+        return s + "\n" + spaq + "; \n" + spaqof + "; \n" + sof + "; \n" + spof + "}";
     }
 }
